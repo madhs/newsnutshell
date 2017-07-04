5dec56e991dffe04567a1f61428ec4ab81e86c8c
madhs <madhavan.8@gmail.com>
2017-07-05 03:33:04 +0800

    New MAX_WIDTH param
    - Added a new param that will allow the RSS link description to be
    wrapped upto that limit. Default value of 60 characters
    - Added checking for empty xml response

--- newsnut.php
+++ newsnut.php
@@ -37,9 +37,8 @@ $CUTOFF_TIME = time() - ($hrs * 60 * 60);
 $LAST_BUILD = array();
 
 $sleep_duration = isset($INI['FREQ']) ? (int) $INI['FREQ'] : 120;
-
-$STREAM_SPEED = isset($INI['MAIN']['STREAM_SPEED']) ? $INI['MAIN']['STREAM_SPEED'] * 1000000 : 0.05 * 1000000;
-
+$STREAM_SPEED = isset($INI['MAIN']['STREAM_SPEED']) ? $INI['MAIN']['STREAM_SPEED'] * 1000000 : (0.05 * 1000000);
+$MAX_WIDTH = isset($INI['MAIN']['MAX_WIDTH']) ? (int) $INI['MAIN']['MAX_WIDTH'] : 60;
 
 $rcx = new RollingCurlX(10);
 
@@ -80,8 +79,9 @@ function parse_rss_result($response, $url, $request_info, $user_data, $time) {
 	}
 
 	$channel_title = (isset($xml->channel->title)) ? $xml->channel->title : null;
+	$item_array = (isset($xml->channel->item)) ? $xml->channel->item : array();
 
-	foreach ($xml->channel->item as $item) {
+	foreach ($item_array as $item) {
 		$date_str = $item->pubDate; // Sat, 01 Jul 2017 12:34:21 GMT
 		if ($date_str) {
 			if (!isset($date_format[$user_data]) || !isset($timezone[$user_data])) {
@@ -110,13 +110,14 @@ function parse_rss_result($response, $url, $request_info, $user_data, $time) {
 
 			$msg = "\e[96m$channel_title\e[0m [{$d2->format('D, d M Y H:i:s')}] [$user_data]" . PHP_EOL
 			. "\e[91m" . "## {$item->title} ## " . "\e[0m" . PHP_EOL;
-			$desc = "\e[1m" . clean_string($item->description) . "\e[0m";
-			$msg .=  !empty($desc) ? "\t$desc" . PHP_EOL . PHP_EOL : null;
+			$desc = /*"\e[1m" .*/ clean_string($item->description) /*. "\e[0m"*/;
+			$msg .=  !empty($desc) ? "\t $desc" . PHP_EOL . PHP_EOL : null;
 			$link = isset($item->guid) ? $item->guid : (isset($item->link) ? $item->link : null);
 			if ($link) {
 				$link = "\e[2m" . $link . "\e[0m";
 			}
-			$msg .= !empty(trim($link)) ? trim($link) . PHP_EOL : null;
+			$trim_text = trim($link);
+			$msg .= !empty($trim_text) ? $trim_text . PHP_EOL : null;
 			$NEWS[$t] = $msg;
 		}
 	}
@@ -147,23 +148,42 @@ function check_special_timeformat($ref) {
 }
 
 function print_news($news) {
-	global $STREAM_SPEED;
+	global $MAX_WIDTH, $STREAM_SPEED;
 	ksort($news);
 	foreach($news as $n) {
 		$arr = explode(PHP_EOL, $n);
-		foreach ($arr as $k => $s) {
-			$trim_line = ($k === 0 || strpos($s, '##') !== false) ? false : true;
-			$a = str_split($s, 1);
-			$i = 1;
-			foreach ($a as $l) {
-				echo $l;
-				$i += 1;
-				if ($trim_line && $i > 60 && $l == ' ') {
-					echo PHP_EOL;
-					$i = 1;
+		foreach ($arr as $key => $line) {
+			// Do not trim the info line or the headline that starts with ##
+			$trim_line = ($key === 0 || strpos($line, '##') !== false) ? false : true;
+			$words = explode(' ', $line);
+			$length = 0;
+			echo "\e[1m";
+			foreach ($words as $k => $w) {
+				$l = str_split($w, 1);
+				foreach ($l as $i) {
+					if ($i == '') continue;
+					echo ($i == "\t") ? '     ' : $i;
+					$length += ($i == "\t" ? 5 : 1);
+					usleep($STREAM_SPEED);
 				}
-				usleep($STREAM_SPEED);
+				if ($trim_line) {
+					$next_word_length = isset($words[$k+1]) ? strlen($words[$k+1]) : 0;
+					// echo ' <' . $length . ' + 1 + ' . $next_word_length . ' "' . $words[$k+1] . '">' . PHP_EOL; var_dump($words[$k+1]);
+					// Length of current word, a whitespace in between, and the next
+					if ($length + 1 + $next_word_length >= $MAX_WIDTH) {
+						echo PHP_EOL;
+						$length = 0;
+					}
+				}
+				// $length = 0 would mean that the cursor is in a new line,
+				// if so, do not start the newline with a space
+				if ($length != 0) {
+					echo ' ';
+					$length += 1;					
+					usleep($STREAM_SPEED);
+				}
 			}
+			echo "\e[0m";
 			echo PHP_EOL;
 		}
 	}
